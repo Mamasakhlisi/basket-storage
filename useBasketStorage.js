61b488fdbf2d19b72018cf10775a0d61d5c79cc4
Mamasakhlisi <datomamasakhlisi@gmail.com>
2021-07-31 09:42:02 -0700

    feat file

---
+++ useBasketStorage.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+function getSavedBasket() {
+  const savedBasket = JSON.parse(localStorage.getItem("basket"));
+  if (savedBasket) return savedBasket;
+}
+
+const useBasketStorage = () => {
+  const [basket, setBasket] = useState(() => {return getSavedBasket()})
+
+  function setBasketItem (item) {
+    console.log(!basket.length)
+    console.log(    basket?.map(x => x.id !== item.id && x.id))
+    }
+
+  function findBasketItem(id) {
+    basket.find((item) => item.id === id);
+  }
+
+  function removeBasketItem(id) {
+    setBasket(basket.filter((item) => item.id !== id));
+  }
+
+  function clearBasket() {
+    setBasket([]);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("basket", JSON.stringify(basket));
+  }, [basket]);
+
+  return [basket,setBasketItem, removeBasketItem, findBasketItem, clearBasket];
+};
+
+export default useBasketStorage;
